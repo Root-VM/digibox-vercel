39abfa1ed0b40f8cd41b79ad51747b6fa228d58b
Vlad <ngvmtext@gmail.com>
2023-02-16 14:23:37 +0200

    fix build

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,4 +3,5 @@ export const environment = {
    production: false,
    API_KEY: "d4bc1bab78b8c853de5b9e6ea3f75559cbb595b01ccf5e8f3496dbc4619102760f8c5634e0f49cb3ef5c2bdf187a12dc60d9932cc8cb1a5759e4ef88e5801c17a267cc9df7507de68fabafc4285857c2151643f89dd54a1ae75ed1af45c6a7c7e68b2cc6d39f263996b5bdd0514053c094669ecfb5204d1a105ce55ca024c5be",
    API_URL: "https://digibox-cms.nadlo.pp.ua/"
+
 };
